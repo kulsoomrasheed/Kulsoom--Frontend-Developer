113796e07c7fd3ed8da4c6870a92e303bc4cacbc
kulsoomrasheed <rasheedamaan111@gmail.com>
2024-03-07 05:27:43 +0530

    added introduction box

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -12,7 +12,7 @@ const Intro = () => {
           <Text fontFamily="ZCOOL KuaiLe" fontWeight={400} fontSize="1.5rem" lineHeight="1.8rem" color="white" maxW="35rem" marginBottom="2rem">
             We've all been in the mud once, and now we've decided to fight it out. Pay tribute to those pioneers of WEB3 and the warriors who dedicated their love to the blockchain. Save the lucky ones alive, join us to save the future! Our mission is to empower everyone to share wealth and succeed. read more...
           </Text>
-          <Button colorScheme="red" textAlign={'left'} size="lg">Join Us</Button> {/* Added red button */}
+          <Button colorScheme="red" textAlign={'left'} size="lg">Join Us</Button> 
         </Box>
         <Box display="flex" justifyContent="center">
           <Image maxH="45.75rem" maxW="45.75rem" src="intro.png" />
